516dce1c5d9336b3232016cabd610681b41fb248
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-03-23 07:17:27 +0600

    Add local storage function

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -16,6 +17,7 @@ const Shop = () => {
     const handleAddToCart = (product) =>{
         const newCart = [...cartItem, product];
         setCartItem(newCart);
+        addToDb(product.id);
     }
 
     return (
--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -39,4 +39,4 @@ export {
     addToDb, 
     removeFromDb,
     deleteShoppingCart
-}
+};
