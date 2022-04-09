3500cb9bec5f3afcde5cc9baab2090aa0cdf3e77
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-10 00:32:59 +0600

    get input value in sign up page

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleEmailField = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordField = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleConfirmPasswordField = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Email : ", email);
+    console.log("Password : ", password);
+    console.log("Confirm Password : ", confirmPassword);
+  };
+
   return (
     <div className="form-container">
       <h1 className="form-header">Sign Up</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="input-group">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" />
+          <input onBlur={handleEmailField} type="email" name="email" />
         </div>
         <div className="input-group">
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" />
+          <input onBlur={handlePasswordField} type="password" name="password" />
         </div>
         <div className="input-group">
           <label htmlFor="confirm-password">Confirm Password</label>
-          <input type="password" name="confirm-password" />
+          <input
+            onBlur={handleConfirmPasswordField}
+            type="password"
+            name="confirm-password"
+          />
         </div>
         <input type="submit" value="Sign Up" className="form-btn" />
         <p className="form-link-text">
